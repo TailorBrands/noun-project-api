6685bb07981ccc21dbe745dfba25361d62b7205f
Nadav Shatz <nadavshatz@gmail.com>
2014-11-05 20:54:22 +0200

    Specs update

--- spec/lib/noun-project-api/retriever_spec.rb
+++ spec/lib/noun-project-api/retriever_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe NounProjectApi::Retriever do
   end
 
   context "Icon" do
-    it 'raises an error when no phrase is provided'
-    it 'returns a proper result with a correct phrase'
-    it 'returns an empty result with a missing phrase'
+    it 'raises an error when no id is provided'
+    it 'returns a proper result with a correct id'
+    it 'returns an empty result with a missing id'
   end
 end
