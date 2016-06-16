81b4d232a803f33a46a3e2ad1eacc7c75872fed0
Nadav Shatz <nadavshatz@gmail.com>
2016-06-16 10:43:21 -0400

    Set the hound ci settings

--- lib/noun-project-api/base_item.rb
+++ lib/noun-project-api/base_item.rb
@@ -8,7 +8,9 @@ module NounProjectApi
       fail(NotImplementedError, "Must use a subclass") if self.class::ITEM_NAME.nil?
 
       origin = JSON.parse(origin) if origin.is_a? String
-      origin = origin.delete(self.class::ITEM_NAME) if origin.key? self.class::ITEM_NAME
+      origin = origin.delete(
+        self.class::ITEM_NAME
+      ) if origin.key? self.class::ITEM_NAME
 
       @original_hash = origin
     end
--- spec/lib/noun-project-api/base_item_spec.rb
+++ spec/lib/noun-project-api/base_item_spec.rb
@@ -4,6 +4,8 @@ require "ostruct"
 RSpec.describe NounProjectApi::BaseItem do
   it "raises an error on direct initialization" do
     data = JSON.parse(Fakes::Results::ICON_VALID)
-    expect { NounProjectApi::BaseItem.new(JSON.dump(data["icon"])) }.to raise_error(NotImplementedError)
+    expect {
+      NounProjectApi::BaseItem.new(JSON.dump(data["icon"]))
+    }.to raise_error(NotImplementedError)
   end
 end
